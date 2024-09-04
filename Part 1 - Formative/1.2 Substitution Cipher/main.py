e21587867f2571e65deab849cc503fa2bf681de2
185933 <hb.bulb91@gmail.com>
2024-09-04 11:07:44 -0400

    finished 1.2 Substitution Cipher 09/04/24

--- Part 1 - Formative/1.2 Substitution Cipher/main.py
+++ Part 1 - Formative/1.2 Substitution Cipher/main.py
@@ -4,11 +4,21 @@ alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
 def sub_encode(text, codebet):
-    return ""
+    encodedWord = ""
+    for i in range(len(text)):
+        for j in range(len(alpha)):
+            if text[i] == alpha[j]:
+                encodedWord = encodedWord + codebet[j]
+    return encodedWord
 
 
 def sub_decode(text, codebet):
-    return ""
+    decodedWord = ""
+    for i in range (len(text)):
+        for j in range(len(codebet)):
+            if text[i] == codebet[j]:
+                decodedWord = decodedWord + alpha[j]
+    return decodedWord
 
 
 test = "HELLOWORLD"
