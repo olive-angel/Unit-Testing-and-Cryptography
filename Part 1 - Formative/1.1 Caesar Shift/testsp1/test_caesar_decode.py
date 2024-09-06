320c8cfdb8fdb6535df25820c68c539f6f8f1ca8
185933 <hb.bulb91@gmail.com>
2024-09-06 11:09:25 -0400

    made test methods 09/06/24

--- Part 1 - Formative/1.1 Caesar Shift/testsp1/test_caesar_decode.py
+++ Part 1 - Formative/1.1 Caesar Shift/testsp1/test_caesar_decode.py
@@ -5,12 +5,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from main import insert_string
 class TestCaesarDecode(TestCase):
-    def test_caesar_decode_
-
-    def test_caesar_decode_
-
-    def test_caesar_decode_
-
-    def test_caesar_decode
-
-    def test_caesar_decode_
+    def test_caesar_decode_two_parameters_with_uppercase(self):
+        self.assertEqual()
+    def test_caesar_decode_two_parameters_with_lowercase(self):
+        self.assertEqual()
--- Part 1 - Formative/1.1 Caesar Shift/testsp1/test_caesar_encode.py
+++ Part 1 - Formative/1.1 Caesar Shift/testsp1/test_caesar_encode.py
@@ -6,12 +6,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from main import insert_string
 class TestCaesarEncode(TestCase):
 
-    def test_caesar_encode_
+    def test_caesar_encode_with_two_parameters(self):
+        self.assertEqual
 
-    def test_caesar_encode_
-
-    def test_caesar_encode_
-
-    def test_caesar_encode
-
-    def test_caesar_encode_
+    def test_caesar_encode_with_two_parameters(self):
+        self.assertEqual
