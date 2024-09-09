9fef89eb9188c2f1581618be785ed1db9d13a7d6
185933 <hb.bulb91@gmail.com>
2024-09-09 11:06:53 -0400

    edited test methods 09/09/24

--- Part 1 - Formative/1.1 Caesar Shift/testsp1/test_caesar_decode.py
+++ Part 1 - Formative/1.1 Caesar Shift/testsp1/test_caesar_decode.py
@@ -10,6 +10,9 @@ class TestCaesarDecode(TestCase):
     def test_caesar_decode_two_parameters_with_lowercase(self):
         self.assertEqual(caesar_decode("mjqqtbtwqi", 5), "HELLOWORLD")
 
-    def test_caesar_encode_with_
-
-    def test_caesar_encode_with_
+    def test_caesar_encode_with_empty_second_parameter(self):
+        self.assertEqual
+    def test_caesar_encode_with_empty_first_parameter(self):
+        self.assertEqual
+    def test_caesar_encode_whitespace(self):
+        self.assertEqual
