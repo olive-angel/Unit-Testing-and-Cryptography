5c9683aaf3d368a4afa438c3beabe58aacefb22b
185933 <hb.bulb91@gmail.com>
2024-09-10 10:46:10 -0400

    added test methods for 1.2 09/10/24

--- Part 1 - Formative/1.2 Substitution Cipher/testsp2/test_sub_decode.py
+++ Part 1 - Formative/1.2 Substitution Cipher/testsp2/test_sub_decode.py
@@ -3,15 +3,20 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main import insert_string
+from main import sub_decode
 
 class TestSubDecode(TestCase):
-    def test_sub_decode_
+    def test_sub_decode_two_strings_with_uppercase(self):
+        self.assertEqual(sub_decode("HELLOWORLD","WJKUXVBMIYDTPLHZGONCRSAEFQ"),  "MXTTHAHOTU")
 
-    def test_sub_decode_
+    def test_sub_decode_two_strings_with_lowercase(self):
+        self.assertEqual(sub_decode("")
 
-    def test_sub_decode_
+    def test_sub_decode_with_empty_second_parameter(self):
+        self.assertEqual(sub_decode
 
-    def test_sub_decode
+    def test_sub_decode_with_empty_first_parameter(self):
+        self.assertEqual(sub_decode
 
-    def test_sub_decode_
+    def test_sub_decode_with_whitespace(self):
+        self.assertEqual(sub_decode
--- Part 1 - Formative/1.2 Substitution Cipher/testsp2/test_sub_encode.py
+++ Part 1 - Formative/1.2 Substitution Cipher/testsp2/test_sub_encode.py
@@ -3,15 +3,20 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main import insert_string
+from main import sub_encode
 
 class TestSubEncode(TestCase):
-    def test_sub_encode_
+    def test_sub_encode_two_strings_with_uppercase(self):
+        self.assertEqual(sub_encode
 
-    def test_sub_encode_
+    def test_sub_encode_two_strings_with_lowercase(self):
+        self.assertEqual(sub_encode
 
-    def test_sub_encode_
+    def test_sub_encode_with_empty_second_parameter(self):
+        self.assertEqual(sub_encode
 
-    def test_sub_encode
+    def test_sub_encode_with_empty_first_parameter(self):
+        self.assertEqual(sub_encode
 
-    def test_sub_encode_
+    def test_sub_encode_with_whitespace(self):
+        self.assertEqual(sub_encode
